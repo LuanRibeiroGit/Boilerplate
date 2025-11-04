a73bac1f85c1a3427b95f7c7903a132964c85b52
LuanRibeiroGit <luancoxa2009@gmail.com>
2025-11-04 11:34:05 -0300

    feat(cta): estruturação do DTO

--- src/modules/cta/dto/create-cta.dto.ts
+++ src/modules/cta/dto/create-cta.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCtaDto {}
+import { IsString, IsEmail } from "class-validator";
+
+export class CreateCtaDto {
+    @IsString({})
+    name: string
+
+    @IsEmail({}, { message: 'Digite o e-mail com o formato correto. Ex...: Exemplo@newdawn.com' })
+    email: string
+
+    @IsString({})
+    company: string
+
+    @IsString({})
+    intention: string
+}
